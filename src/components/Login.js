47f591480c5c596419b7621b510de2037074dc2a
maria daniela <orregodaniela8@gmail.com>
2023-07-05 09:21:56 -0500

    cambios 5

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,30 @@
 export const Login = (onNavigate) => {
-  const HomeDiv = document.createElement('div');
-  HomeDiv.textContent = 'Bienvenida a Login';
-  const buttonHome = document.createElement('button');
-  buttonHome.textContent = 'Regresar al Home';
+  const loginDiv = document.createElement('div');
+  loginDiv.textContent = 'Bienvenido a la página de inicio de sesión';
 
-  buttonHome.addEventListener('click', () => onNavigate('/'));
-  HomeDiv.appendChild(buttonHome);
-  return HomeDiv;
+  const emailInput = document.createElement('input');
+  emailInput.type = 'email';
+  emailInput.placeholder = 'Correo electrónico';
+  loginDiv.appendChild(emailInput);
+
+  const passwordInput = document.createElement('input');
+  passwordInput.type = 'password';
+  passwordInput.placeholder = 'Contraseña';
+  loginDiv.appendChild(passwordInput);
+
+  const loginButton = document.createElement('button');
+  loginButton.textContent = 'Iniciar sesión';
+  loginButton.addEventListener('click', () => {
+    // Aquí puedes realizar la lógica de autenticación y redireccionamiento
+    // a la página correspondiente después de iniciar sesión
+
+  });
+  loginDiv.appendChild(loginButton);
+
+  const backHomeButton = document.createElement('button');
+  backHomeButton.textContent = 'Regresar al Home';
+  backHomeButton.addEventListener('click', () => onNavigate('/'));
+  loginDiv.appendChild(backHomeButton);
+
+  return loginDiv;
 };
