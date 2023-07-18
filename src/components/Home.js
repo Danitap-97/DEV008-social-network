f367ea11f451c0b9b2831b23da27b13d2e71dc39
nancygonzalezrex <nancygonzalezrex@gmail.com>
2023-07-18 12:51:10 -0400

    cambios de home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,12 @@ export const Home = (onNavigate) => {
   section.classList.add('section-home');
   const logoContainer = document.createElement('div');
   logoContainer.classList.add('logo-container');
+  const modalSomos = document.createElement('article');
+  modalSomos.classList.add('modal-somos');
+  modalSomos.textContent = 'Bienvenidos a nuestra emocionante red social de viajes! Somos un equipo apasionado de viajeros y entusiastas de la aventura, Nuestro objetivo es brindarte una comunidad dinámica donde puedas compartir tus propias experiencias de viaje, conectarte con otros viajeros, buscar recomendaciones y descubrir destinos fascinantes que quizás no hayas considerado antes. Queremos que te sientas inspirado y empoderado para explorar el mundo y crear tu propia historia de viaje, En nuestra red social de viajes, puedes crear un perfil personalizado para mostrar tus destinos favoritos, tus fotos más impactantes y tus consejos más valiosos. Puedes conectarte con otros viajeros, seguir sus perfiles y descubrir sus aventuras,';
 
   const logoImage = document.createElement('img');
-  logoImage.src = 'https://firebasestorage.googleapis.com/v0/b/social-network-e2319.appspot.com/o/Blue___Yellow_Minimal_Travel_Agency_Free_Logo-removebg-preview.png?alt=media&token=777e5a3b-7cdc-4bec-980a-d160947cd0fb'; // Reemplaza 'ruta-de-la-imagen/logo.png' con la ruta correcta de tu imagen
+  logoImage.src = 'https://firebasestorage.googleapis.com/v0/b/social-network-2-293be.appspot.com/o/Blue%20%26%20Yellow%20Minimal%20Travel%20Agency%20Free%20Logo.png?alt=media&token=cd186baa-c430-4feb-a010-e5cfa600dfdd'; // Reemplaza 'ruta-de-la-imagen/logo.png' con la ruta correcta de tu imagen
   logoImage.alt = 'Logo de la página web';
   logoContainer.appendChild(logoImage);
 
@@ -18,10 +21,22 @@ export const Home = (onNavigate) => {
   buttonRegister.classList.add('button-register');
   const buttonLogin = document.createElement('button');
   buttonLogin.classList.add('button-login');
+  const buttonSomos = document.createElement('button');
+  buttonSomos.classList.add('button-somos');
   const footerHome = document.createElement('footer');
   footerHome.classList.add('footer-home');
   footerHome.textContent = 'Copyright © 2023 Traveling Friends, Inc. All rights reserved.';
+  function openModal() {
+    const modalSomosFlex = modalSomos.style.display === 'flex';
+    if (modalSomosFlex) {
+      modalSomos.style.display = 'none';
+    } else {
+      modalSomos.style.display = 'flex';
+    }
+  }
 
+  buttonSomos.textContent = 'Quienes Somos';
+  buttonSomos.addEventListener('click', openModal);
   buttonRegister.textContent = 'Registrate';
   buttonRegister.addEventListener('click', () => onNavigate('/register'));
   const registrado = document.createElement('p');
@@ -31,9 +46,11 @@ export const Home = (onNavigate) => {
 
   homeDiv.appendChild(section);
   section.appendChild(navHome);
+  section.appendChild(modalSomos);
   homeDiv.appendChild(footerHome);
   navHome.appendChild(logoContainer);
   navHome.appendChild(buttonContainer);
+  buttonContainer.appendChild(buttonSomos);
   buttonContainer.appendChild(buttonRegister);
   buttonContainer.appendChild(buttonLogin);
   return homeDiv;
--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 // TODO: Replace the following with your app's Firebase project configuration
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyATBUTgJTxigajaUl_JKQI5OUkFigfNOpM',
-  authDomain: 'social-network-e2319.firebaseapp.com',
-  projectId: 'social-network-e2319',
-  storageBucket: 'social-network-e2319.appspot.com',
-  messagingSenderId: '338428955083',
-  appId: '1:338428955083:web:676d5a2fa2484096cbce4e',
-  measurementId: 'G-77JZQN59HW',
+  apiKey: 'AIzaSyBYKTgzTOfwLkqIwUgUYjaa01SenkYMW6k',
+  authDomain: 'social-network-2-293be.firebaseapp.com',
+  projectId: 'social-network-2-293be',
+  storageBucket: 'social-network-2-293be.appspot.com',
+  messagingSenderId: '742971928202',
+  appId: '1:742971928202:web:d06cdcbd266de295f18773',
+  measurementId: 'G-YMDMY4MJV8',
 };
 
 const app = initializeApp(firebaseConfig);
