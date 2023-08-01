ecc8e624a6cab815066d636c1d0fb7f1e7b7ca25
nancygonzalezrex <nancygonzalezrex@gmail.com>
2023-08-01 12:37:34 -0400

    cambios css en landing

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -6,6 +6,7 @@ import {
   query,
   orderBy,
   onSnapshot,
+  limit,
 } from 'firebase/firestore';
 import { db } from './firebase';
 
@@ -17,8 +18,7 @@ export const docRef = (contenido, fecha, nombre) => addDoc(collection(db, 'post'
 });
 
 /* Función que lista los posts y cuando se agrega un nuevo post se vuela a ejecutar */
-export const onGetPosts = (callback) => onSnapshot(query(collection(db, 'post'), orderBy('fecha', 'desc')), callback);
-
+export const onGetPosts = (callback) => onSnapshot(query(collection(db, 'post'), orderBy('fecha', 'desc'), limit(1000)), callback);
 /* Función que elimina el post por id */
 export const deletePost = (id) => deleteDoc(doc(db, 'post', id));
 
