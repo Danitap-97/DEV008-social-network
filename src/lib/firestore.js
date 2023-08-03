784db914b371f92c9a624ab33f761705f863d2ed
nancygonzalezrex <nancygonzalezrex@gmail.com>
2023-08-03 10:51:07 -0400

    se hace merge con cambios actuales

--- src/lib/firestore.js
+++ src/lib/firestore.js
@@ -7,6 +7,7 @@ import {
   orderBy,
   onSnapshot,
   updateDoc,
+  setDoc,
 } from 'firebase/firestore';
 import { db } from './firebase';
 
@@ -18,13 +19,14 @@ export const docRef = (contenido, fecha, nombre) => addDoc(collection(db, 'post'
 });
 
 /* Función que lista los posts y cuando se agrega un nuevo post se vuela a ejecutar */
-export const onGetPosts = (callback) => onSnapshot(query(collection(db, 'post'), orderBy('fecha', 'desc'), limit(1000)), callback);
+export const onGetPosts = (callback) => onSnapshot(query(collection(db, 'post'), orderBy('fecha', 'desc')), callback);
 /* Función que elimina el post por id */
 export const deletePost = (idPost) => deleteDoc(doc(db, 'post', idPost));
 
 export const updateLike = (idPost, likes) => {
   const postSelected = doc(db, 'post', idPost);
+  // eslint-disable-next-line no-use-before-define
   return updateDoc(postSelected, { likes });
 };
 
-export const upDateDoc = (id) => upDateDoc(doc(db, 'post', id));
+export const upDateDoc = (idPost, nuevoContenido) => setDoc(doc(db, 'post', idPost), { contenido: nuevoContenido }, { merge: true });
